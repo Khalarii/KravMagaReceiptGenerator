c35456dbfadf577afec9a472bc5a85e995b6b7e5
Kim Souza <kimgss94@hotmail.com>
2019-07-07 19:13:40 -0300

    Added icon

--- ReciboKravMaga/ReciboKravMaga/Form1.Designer.cs
+++ ReciboKravMaga/ReciboKravMaga/Form1.Designer.cs
@@ -171,6 +171,7 @@
             this.AcceptButton = this.button1;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(414, 261);
             this.Controls.Add(this.paymentDate);
             this.Controls.Add(this.button1);
@@ -181,6 +182,7 @@
             this.Controls.Add(this.studentName);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
