2997b80ccb5982f531202d687ae81ad8129be361
Kim Souza <kimgss94@hotmail.com>
2019-07-07 19:06:19 -0300

    Added installer

--- ReciboKravMaga/ReciboKravMaga/Form1.Designer.cs
+++ ReciboKravMaga/ReciboKravMaga/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.studentName = new System.Windows.Forms.TextBox();
             this.amountPaid = new System.Windows.Forms.TextBox();
@@ -180,6 +181,7 @@
             this.Controls.Add(this.studentName);
             this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
